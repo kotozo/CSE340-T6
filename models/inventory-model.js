1866a0b75a61756ed6144f442aeae0f7d8ac7ded
kotozo <toz20002@byui.edu>
2025-11-14 05:04:36 +0000

    typing err in the query SELECT puplic instead of public

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -4,7 +4,7 @@ const pool = require("../database/index.js")
 * Get all Classification date
 * ************************ */
 async function getClassifications() {
-    return await pool.query("SELECT * FROM puplic.classification ORDER BY classification_name")  
+    return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")  
 }
 
 module.exports = {getClassifications}
