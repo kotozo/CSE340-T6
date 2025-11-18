ef008d1ad4f7fcd3e552114f7c0add865c9a7283
kotozo <toz20002@byui.edu>
2025-11-18 02:46:48 +0000

    server js updated

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message= err.message } else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message: err.message,
