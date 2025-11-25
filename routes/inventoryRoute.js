115df5bf398f23b8f6bee6bc4e241f0168a09e3d
kotozo <toz20002@byui.edu>
2025-11-25 19:41:34 +0000

    inventoryRoute.js reviewed

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,3 +1,4 @@
+/*
 // Needed Resources
 const express = require("express")
 const router = new express.Router()
@@ -11,3 +12,17 @@ router.get("/inv/detail/:inv_id", invController.buildVehicleDetail)
 
 module.exports = router
 
+*/
+
+// Needed Resources
+const express = require("express")
+const router = new express.Router()
+const invController = require("../controllers/invController")
+
+// Route to build inventory by classification view
+router.get("/inv/type/:classificationId", invController.buildByClassificationId)
+
+// Route for single vehicle details
+router.get("/inv/detail/:inv_id", invController.buildVehicleDetail)
+
+module.exports = router
